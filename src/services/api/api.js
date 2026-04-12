f4e272f6c15e5e4601a482e0e8adbf3e44da0e92
willy <willybalt.2017@gmail.com>
2026-04-11 22:21:03 -0400

    consumo de api

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -2,10 +2,8 @@ import axios from "axios";
 
 const URL_BACKEND = "https://dummyjson.com";
 
-export function api() {
-    const api = axios.create({
-        baseURL: URL_BACKEND
-    });
+const api = axios.create({
+    baseURL: URL_BACKEND
+});
 
-    return api
-}
+export { api };
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,7 +1,66 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+import { getProductosRequest } from '../services/products.service';
+
+
+const products = ref([]);
+
+onMounted(async () => {
+    const res = await getProductosRequest();
+    console.log(res.data);
+    products.value = res.data.products;
+    console.log(products)
+})
 
 </script>
 
 <template>
-    <h1>Productos</h1>
+    <h1 class="text-3xl font-bold text-center my-6">Productos</h1>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-6">
+
+        <div v-for="p in products" :key="p.id"
+            class="bg-white rounded-2xl shadow-md hover:shadow-xl transition overflow-hidden">
+
+            <!-- Imagen -->
+            <img :src="p.images[0]" alt="" class="w-full h-48 object-cover" />
+
+            <!-- Contenido -->
+            <div class="p-4 flex flex-col gap-2">
+
+                <!-- Título -->
+                <h2 class="font-semibold text-lg line-clamp-1">
+                    {{ p.title }}
+                </h2>
+
+                <!-- Descripción -->
+                <p class="text-sm text-gray-500 line-clamp-2">
+                    {{ p.description }}
+                </p>
+
+                <!-- Precio + rating -->
+                <div class="flex justify-between items-center mt-2">
+                    <span class="text-green-600 font-bold text-lg">
+                        ${{ p.price }}
+                    </span>
+                    <span class="text-yellow-500 text-sm">
+                        ⭐ {{ p.rating }}
+                    </span>
+                </div>
+
+                <!-- Extra -->
+                <div class="flex justify-between text-xs text-gray-400">
+                    <span>Stock: {{ p.stock }}</span>
+                    <span class="capitalize">{{ p.category }}</span>
+                </div>
+
+                <!-- Botón -->
+                <button class="mt-3 bg-blue-500 hover:bg-blue-600 text-white py-2 rounded-lg transition">
+                    Comprar
+                </button>
+
+            </div>
+        </div>
+
+    </div>
 </template>
